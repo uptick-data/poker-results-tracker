92c8b08a315d5639556673e4b1de485c7c2b6e7b
Shuming Peh <shumingpeh@up-tick.org>
2021-03-09 00:33:52 +0800

    include option to filter top 8 players, remove zero sessions

--- src/pages/poker_results_charts.py
+++ src/pages/poker_results_charts.py
@@ -19,6 +19,27 @@ def write():
     # read csv
     df = read_csv_file(settings.master_file_directory, settings.master_file)
 
+    # filter out sessions that are all zeros
+    df_non_zero = (
+        df
+        .groupby(["date", "result"])
+        .agg({"place":"count","user_name":"nunique"})
+        .reset_index()
+        .pipe(lambda x:x.assign(percentage_zeros = x.place/x.user_name))
+        .pipe(lambda x:x.assign(is_exclude_date = np.where(
+            (x.percentage_zeros >= 0.75) & (x.result == 0), 1, 0
+        )))
+        .query("is_exclude_date != 1")
+        [["date"]]
+        .drop_duplicates()
+    )
+
+    df = (
+        df
+        .merge(df_non_zero, how="inner", on=['date'])
+        .pipe(lambda x:x.assign(is_win = np.where(x.result >= 0.02, 1, 0)))
+    )
+
     # cumulative results overtime
     df_cumulative = df.sort_values(["user_name", "date"])
     df_cumulative["cumsum_result"] = df_cumulative.groupby(
@@ -27,18 +48,33 @@ def write():
 
     # top results as of today
     df_top = (
-        df.query("result != 0")
+        df
+        .query("result != 0")
         .groupby(["user_name"])
-        .agg({"result": "sum", "date": "nunique"})
+        .agg({"result": "sum", "date": "nunique", "is_win": "sum"})
         .reset_index()
         .pipe(lambda x: x.assign(avg_per_session=x.result / x.date))
         .rename(columns={"date": "num_sessions"})
         .sort_values(["result", "avg_per_session"], ascending=False)
         .reset_index(drop=True)
+        .pipe(lambda x:x.assign(win_rate = x.is_win/x.num_sessions))
+        .drop(['is_win'], 1)
+        [['user_name', 'result', 'num_sessions', 'win_rate', 'avg_per_session']]
     )
 
+    # get total number of sessions
+    total_num_sessions = range(df_top.num_sessions.max()+1)[1:]
+
     st.text("Top 8 results as of today, " + str(datetime.datetime.now()))
-    st.write(df_top.head(8))
+    is_remove_few = st.selectbox("Select players with >= number of sessions?", tuple(total_num_sessions))
+
+    df_top_filter = (
+        df_top
+        # .query(f"num_sessions >= '{is_remove_few}'")
+        .query("num_sessions >= " + str(is_remove_few))
+        .reset_index(drop=True)
+    )
+    st.write(df_top_filter.head(8))
 
     # get all dates
     list_of_dates = ["All"]
